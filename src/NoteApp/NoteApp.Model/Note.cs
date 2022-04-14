1c32667ec76964fba8055e2f3cf7ab51affed8b7
Максим Вишняков <vishhhnyakovmi@gmail.com>
2022-04-14 18:51:18 +0700

    (Note, NoteCategory, Project):  добавлены и реализованы классы

--- src/NoteApp/NoteApp.Model/Note.cs
+++ src/NoteApp/NoteApp.Model/Note.cs
@@ -1,72 +1,136 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Note : ICloneable
+namespace NoteApp.Model
 {
-	private string _name = "Без названия" { get; set; }
-
-	private string _categoryNote { get; set; }
-
-	private string _textNote { get; set; }
-
-	private DateTime _creationDate { get; set; }
-
-	private DateTime _lastModifiedTime { get; set; }
-
-	public Note(string name, string categoryNote, string textNote, DateTime creationDate)
-    {
-		SetName(string name);
-		SetCategoryNote(string categoryNote);
-		SetTextNote(string textNote);
-		_creationDate = creationDate;
-	}
-
-	public void SetName (string name, DateTime lastModifiedTime)
-    {
-		_name = name;
-		_lastModifiedTime = lastModifiedTime;
-
-	}
-
-	public void SetCategoryNote(string categoryNote, DateTime lastModifiedTime)
+	/// <summary>
+	/// Описывает заметку.
+	/// </summary>
+	public class Note : ICloneable
 	{
-		_categoryNote = categoryNote;
-		_lastModifiedTime = lastModifiedTime;
+		/// <summary>
+		/// Название заметки.
+		/// </summary>
+		private string _name = "Без названия";
+
+		/// <summary>
+		/// Категория заметки.
+		/// </summary>
+		private NoteCategory _category;
+
+		/// <summary>
+		/// Текст заметки.
+		/// </summary>
+		private string _text;
+
+		/// <summary>
+		/// Дата создания заметки.
+		/// </summary>
+		private DateTime _creationDate = DateTime.Now;
+
+		/// <summary>
+		/// Время последнего изменения заметки.
+		/// </summary>
+		private DateTime _lastModifiedTime;
+
+		/// <summary>
+		/// Задает и возвращает имя заметки.
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if (value.Length > 50)
+				{
+					throw new ArgumentException(
+						$"The number of letters in the title must be less than 50"
+						+ $"But was {_name.Length}");
+				}
+				_name = value;
+				_lastModifiedTime = DateTime.Now;
+			}
+		}
+
+		/// <summary>
+		/// Задает и возвращает категорию заметки.
+		/// </summary>
+		public NoteCategory Category
+		{
+			get
+			{
+				return _category;
+			}
+			set
+			{
+				_category = value;
+				_lastModifiedTime = DateTime.Now;
+			}
+		}
+
+		/// <summary>
+		/// Задает и возвращает текст заметки.
+		/// </summary>
+		public string Text
+		{
+			get
+			{
+				return _text;
+
+			}
+			set
+			{
+				_text = value;
+				_lastModifiedTime = DateTime.Now;
+			}
+		}
+
+		/// <summary>
+		/// Возвращает дату создания заметки.
+		/// </summary>
+		public DateTime CreationDate
+		{
+			get
+			{
+				return _creationDate;
+			}
+		}
+
+		/// <summary>
+		/// Возвращает время послденего изменения заметки.
+		/// </summary>
+		/// <returns></returns>
+		public DateTime LastModifiedTime
+		{
+			get
+			{
+				return _lastModifiedTime;
+			}
+		}
+
+		/// <summary>
+		/// Создает экземпляр заметки.
+		/// </summary>
+		public Note(string name, NoteCategory category, string text)
+		{
+			Name = name;
+			Category = category;
+			Text = text;
+		}
+
+		/// <summary>
+		/// Метод копирования.
+		/// </summary>
+		/// <returns></returns>
+		public object Clone()
+		{
+			return new Note(Name, Category, Text);
+		}
 	}
-
-	public void SetTextNote(string textNote, DateTime lastModifiedTime)
-    {
-		_textNote = textNote;
-		_lastModifiedTime = lastModifiedTime;
-	}
-
-	public string GetName()
-    {
-		return _name;
-    }
-
-	public string GetCategoryNote()
-	{
-		return _categoryNote;
-	}
-
-	public string GetTextNote()
-	{
-		return _textNote;
-	}
-
-	public DateTime GetCreationDate()
-    {
-		return _creationDate;
-    }
-
-	public DateTime GetLastModifiedTime()
-	{
-		return _creationDate;
-	}
-
-	public object Clone()
-    {
-		return new Note(string name, string categoryNote, string textNote, 
-			DateTime creationDate);
-    }
 }
---
+++ src/NoteApp/NoteApp.Model/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Model
+{
+	public class Project
+	{
+		/// <summary>
+		/// Список заметок
+		/// </summary>
+		List<Note> Notes { get; set }
+
+		/// <summary>
+		/// Конструктор класса
+		/// </summary>
+		public Project()
+		{
+			Notes = new List<Note>();
+		}
+	}
+
+}
